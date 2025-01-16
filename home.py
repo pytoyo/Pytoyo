0b8dd0cbaf801a50930a869869a208aa39b8a029
Fabio <fabjolino@gmail.com>
2025-01-15 22:27:10 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -2,3 +2,12 @@ import streamlit as st
 import pandas as pd
 st.title('Sistema contas a pagar a receber')
 
+def home():
+    st.title("Bem-vindo à página inicial!")
+    st.write("Você está logado e acessando a página principal.")
+
+if __name__ == "__main__":
+    if "logged_in" in st.session_state and st.session_state.logged_in:
+        home()
+    else:
+        st.error("Você não está logado. Por favor, faça o login.")
